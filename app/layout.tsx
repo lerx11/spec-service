572244bebc1a0fe8741e72370d5a2709c285910b
Lenar-dev <Le.aloha@mail.ru>
2026-08-23 22:47:39 +0300

    add metrika and fix articles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Montserrat, Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 
@@ -52,8 +53,39 @@ export default function RootLayout({
     <html lang="ru" className={`${montserrat.variable} ${robotoCondensed.variable}`}>
       <body className="font-body bg-white text-cat-black antialiased">
         {children}
-        {/* Яндекс.Метрика — заглушка, будет подключена позже */}
-        {/* <script type="text/javascript">...</script> */}
+
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=111879926', 'ym');
+
+            ym(111879926, 'init', {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: "dataLayer",
+              referrer: document.referrer,
+              url: location.href,
+              accurateTrackBounce: true,
+              trackLinks: true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/111879926"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </body>
     </html>
   );
